163d6028de68bd48d94b781fdbff7d085ca1a503
moemadeldin <mohamedemadeldin34@gmail.com>
2025-11-07 11:30:36 +0200

    testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,17 +5,39 @@ declare(strict_types=1);
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 final class ExampleTest extends TestCase
 {
+    use DatabaseTransactions;
+
+    private string $token;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $response = $this->postJson('/api/login',[
+            'email_or_mobile_or_username' => 'testee2@gmail.com',
+            'password' => 'admin1' 
+        ]);
+        $this->token = $response->json()['data']['access_token'];
+    }
+
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_user_can_delete_comment(): void
     {
-        $response = $this->get('/');
-
+        $comment = Comment::inRandomOrder()->first();
+        $username = $comment?->user?->username;
+        $postId = $comment?->post_id;
+        
+        $response = $this->delete('/api/posts/'.$username.'/'.$postId.'/comment/'.$comment->id,[],['Authorization' => 'Bearer '.$this->token]);
         $response->assertStatus(200);
     }
+    
 }
